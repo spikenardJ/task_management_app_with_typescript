6ad734e1851483106915c50ef7ef536276a197c6
spikenardJ <jennifer_annette@hotmail.com>
2025-01-20 18:00:59 -0600

    Navbar adjustments, removed unwanted pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import TaskDetails from "./components/pages/task/TaskDetails";
 import TaskForm from "./components/pages/task/TaskForm";
 import TaskContextProvider from "./context/TaskContext";
 import { Routes, Route, Navigate } from "react-router-dom";
-// import HomePage from "./components/pages/HomePage";
 import CallbackPage from "./auth0/CallbackPage";
 import ProtectedPage from "./components/pages/ProtectedPage";
 import { useAuth0 } from "@auth0/auth0-react";
 import ProfilePage from "./components/pages/ProfilePage";
 import AuthenticationGuard from "./auth0/AuthenticationGuard";
-import ContentPage from "./components/pages/ContentPage";
 import "./Style.css"
 
 const App: React.FC = () => {
@@ -25,7 +23,6 @@ const App: React.FC = () => {
         <Route path="/" element={<TaskDashboard/>}/>
           <Route path="/profile" element={<AuthenticationGuard component={ProfilePage} />} />
           <Route path="/protected" element={<AuthenticationGuard component={ProtectedPage} />} />
-          <Route path="/content" element={<AuthenticationGuard component={ContentPage} />} />
           <Route path="/callback" element={<CallbackPage />} />
           <Route path="/task/:id" element={<TaskDetails/>} />
           <Route path="/task/new" element={<TaskForm/>} />
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -8,9 +8,10 @@ const NavBar: React.FC = () => {
 return (
   <Navbar bg="light" expand="lg">
     <Navbar.Brand href="/">Task Management App</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
     {isAuthenticated && (
       <>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="nav-bar m-4">
             <NavLink
@@ -30,12 +31,6 @@ return (
               className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}
             >
               Protected Personal Notes
-            </NavLink>
-            <NavLink
-              to="/content"
-              className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}
-            >
-              Content
             </NavLink>
           </Nav>
         </Navbar.Collapse>
